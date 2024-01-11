9639c81397655a5b5c8d4c5781316a1b9aaafc74
mrnorouzi0001 <mrnorouzi2005@gmail.com>
2024-01-11 18:29:57 +0330

    TESTING

--- master/main.c
+++ master/main.c
@@ -320,6 +320,7 @@ void adding_sale_buildings_Residential(){
         system("cls");
         printf("processing");
         Sleep(2000);
+        printf("%s" , building->municipalitys_area);
         fputs(building->municipalitys_area , file_sale_res);
         fputs("\n" , file_sale_res);
         fputs(building->address_of_building , file_sale_res);
@@ -348,8 +349,8 @@ void adding_sale_buildings_Residential(){
         fputs(building->isactive , file_sale_res);
         fputs("\n" , file_sale_res);
         free(building);
-        close(file_ID);
-        close(file_sale_res);
+        free(file_ID);
+        free(file_sale_res);
         system("cls");
         printf("building has added successfully");
         Sleep(2000);
