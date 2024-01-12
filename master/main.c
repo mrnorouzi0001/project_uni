0a4317970fca3562cfae0c4ffcd7ffeec36eee8c
mrnorouzi0001 <mrnorouzi2005@gmail.com>
2024-01-12 11:42:12 +0330

    debuggin

--- master/main.c
+++ master/main.c
@@ -2,6 +2,24 @@
 #include<windows.h>
 void Start_Page();
 void make_list_user(FILE *user_fp);
+struct building_sale{
+    char municipalitys_area[3];
+    char address_of_building[100];
+    char model[20];
+    char age_of_building[5];
+    char size_of_the_infrastructure[10];
+    char amount_of_floors[4];
+    char size_of_the_main_land[10];
+    char phone_number_of_owner[12];
+    char amount_of_rooms[3];
+    char price[13];
+    char user[30];
+    char id[7];
+    char isactive[2];
+    unsigned long int time;
+    struct user *link;
+}*start_user , *last_user;
+
 struct user{
     char user_name[30];
     char password[20];
@@ -10,8 +28,11 @@ struct user{
     char phone[12];
     char email[35];
     struct user *link;
-}*start_user , *last_user;
+}*start_building_sale , *last_building_sale;
+
+typedef struct building_sale BUILDING_SALE;
 typedef struct user USER;
+
 USER *current_user;
 void sign_up(){
     system("cls");
@@ -57,7 +78,7 @@ void sign_up(){
         printf("the user has added to the application successfully!");
         free(user);
         free(user_fp);
-
+        fclose(user_fp);
         Sleep(2000);
         Start_Page();
     }
@@ -75,9 +96,10 @@ void sign_in(){
     if(user_fp == NULL){
         printf("There is a problem with user file. the program will restart itself");
         free(user_fp);
+        fclose(user_fp);
         exit(0);
     }
-   char user_name[30] , password[25];
+    char user_name[30] , password[25];
     printf("Please enter your user name: ");
     gets(user_name);
     strcat(user_name  , "\n");
@@ -92,7 +114,7 @@ void sign_in(){
                 break;
             }
             else{
-                printf("wrong password! %d tries left!\n" , tries);
+                printf("wrong password! %d tries left!\n" , tries + 1);
                 tries++;
             }
         }
@@ -100,23 +122,63 @@ void sign_in(){
         Sleep(2000);
         //make_null_list_user();
         system("cls");
-        free(user_fp);
+        fclose(user_fp);
         main_page();
     }
     else{
         printf("we don't have this user name!");
         Sleep(2000);
-        free(user_fp);
+        fclose(user_fp);
         //make_null_list_user();
         Start_Page();
     }
 }
 void main_page(){
+    int option = 0;
+    system("cls");
     printf("1. Adding new building\n");
     printf("2. Deleting the buildings\n");
     printf("3. Reports\n");
     printf("4. settings\n");
     printf("5. sign out\n");
+    printf("Please choice your option: ");
+    scanf("%d" , &option);
+    getchar();
+    switch(option){
+        case 1:{
+            adding_building();
+            break;
+        }
+        case 2:{
+            delete_building();
+        }
+        case 3:{
+            if(strcmp(current_user->user_name , "admin") == 0){
+                reports_admin();
+                break;
+            }
+            else{
+                reports_normal();
+                break;
+            }
+        }
+        case 4:{
+            user_edit();
+            break;
+        }
+        case 5:{
+            free(current_user);
+            printf("Logging out");
+            Sleep(2000);
+            Start_Page();
+        }
+        default:{
+            printf("Please enter a valid option");
+            Sleep(2000);
+            system("cls");
+            main_page();
+        }
+    }
 }
 void Start_Page(){
     system("cls");
@@ -147,28 +209,157 @@ void Start_Page(){
         }
     }
 }
-void adding_page(){
+void adding_building(){
+    int checker = 0;
+    system("cls");
     printf("1. For sale buildings\n");
     printf("2. For rent buildings\n");
-    printf("3. back");
+    printf("3. back\n");
+    printf("Please choice your option: ");
+    scanf("%d" , &checker);
+    getchar();
+    switch(checker){
+        case 1 :{
+            sale_buildings();
+            break;
+        }
+        case 2 :{
+            rent_buildings();
+            break;
+        }
+        case 3 :{
+            main_page();
+            break;
+        }
+        default: {
+            system("cls");
+            printf("Please enter a valid option");
+            Sleep(1000);
+            adding_building();
+        }
+    }
 }
 void sale_buildings(){
+    system("cls");
+    int checker = 0;
     printf("1. Residential buildings\n");
     printf("2. Commercial\n");
     printf("3. Normal filed\n");
     printf("4. back\n");
+    printf("Please choice your option: ");
+    scanf("%d" , &checker);
+    getchar();
+    switch(checker){
+        case 1 :{
+            adding_sale_buildings_Residential();
+            break;
+        }
+        case 2 :{
+            adding_sale_buildings_commercial();
+            break;
+        }
+        case 3 :{
+            adding_sale_buildings_filed();
+            break;
+        }
+        case 4:{
+            adding_building();
+            break;
+        }
+        default: {
+            system("cls");
+            printf("Please enter a valid option");
+            Sleep(1000);
+            sale_buildings();
+        }
+    }
 }
 void adding_sale_buildings_Residential(){
-    printf("PLease enter the municipality's area: ");
+    int id = 0;
+    char char_id[7] , temp[25];
+    system("cls");
+    FILE *file_sale_res , *file_ID;
+    file_sale_res = fopen("Files\\building\\for_sale\\Residential.txt" , "a+");
+    file_ID = fopen("Files\\building\\for_sale\\ID.txt" , "r+");
+    BUILDING_SALE *building;
+    building = malloc(sizeof(BUILDING_SALE));
+    printf("Please enter the municipality's area: ");
+    gets(building->municipalitys_area);
     printf("Please enter the address of building: ");
-    printf("PLease enter model of building(apartment or Villa): ");
-    printf("PLease enter age of building: ");
+    gets(building->address_of_building);
+    printf("Please enter model of building(apartment or villa): ");
+    gets(building->model);
+    printf("Please enter age of building: ");
+    gets(building->age_of_building);
     printf("Please enter The size of the infrastructure: ");
-    printf("PLease enter amount of floors: ");
-    printf("PLease enter The size of the main land: ");
+    gets(building->size_of_the_infrastructure);
+    printf("Please enter amount of floors: ");
+    gets(building->amount_of_floors);
+    printf("Please enter The size of the main land: ");
+    gets(building->size_of_the_main_land);
     printf("Please enter phone number of owner: ");
-    printf("PLease enter amount of rooms: ");
-    printf("PLease enter price: ");
+    gets(building->phone_number_of_owner);
+    printf("Please enter amount of rooms: ");
+    gets(building->amount_of_rooms);
+    printf("Please enter price: ");
+    gets(building->price);
+    strcpy(building->user , current_user->user_name);
+    building->time = time(NULL);
+    strcpy(building->isactive , "1");
+    fgets(building->id , 7 , file_ID);
+    id = atoi(building->id);
+    id++;
+    itoa(id ,char_id ,10);
+    free(file_ID);
+    file_ID = fopen("Files\\building\\for_sale\\ID.txt" , "w+");
+    fputs(char_id , file_ID);
+    printf("\nAre you sure you want to add this building to your sale list?(Y/N)");
+    if(getchar() == 'Y'){
+        system("cls");
+        printf("processing");
+        Sleep(2000);
+        fputs(building->municipalitys_area , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->address_of_building , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->model , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->age_of_building , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->size_of_the_infrastructure , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->amount_of_floors , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->size_of_the_main_land , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->phone_number_of_owner , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->amount_of_rooms , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->price , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->user , file_sale_res);
+        fputs("\n" , file_sale_res);
+        itoa(building->time , temp , 10);
+        fputs(temp , file_sale_res);
+        fputs("\n" , file_sale_res);
+        fputs(building->isactive , file_sale_res);
+        fputs("\n" , file_sale_res);
+        free(building);
+        fclose(file_ID);
+        fclose(file_sale_res);
+        system("cls");
+        printf("building has added successfully");
+        Sleep(2000);
+        sale_buildings();
+    }
+    else{
+        free(building);
+        fclose(file_ID);
+        fclose(file_sale_res);
+        sale_buildings();
+    }
+
 }
 void adding_sale_buildings_commercial(){
     printf("PLease enter the municipality's area: ");
@@ -350,10 +541,10 @@ void report_user_activity(/*struc User*/){
     printf("Last login: \n");
     printf("Last logout: \n");
 }
-void Delete(){
+void delete_building(){
     //report All
 }
-void user_Edit(){
+void user_edit(){
     printf("Please enter your new name(if don't want to change leave it alone):");
     printf("Please enter your new last name(if don't want to change leave it alone):");
     printf("Please enter your new ID(if don't want to change leave it alone:");
@@ -364,18 +555,18 @@ void user_Edit(){
 }
 void make_list_user(FILE *user_fp){
     USER *temp;
-    temp = malloc(sizeof(USER));
     while(1){
         if(feof(user_fp)){
             break;
         }
+        temp = malloc(sizeof(USER));
         fgets(temp->user_name , 30 , user_fp);
         fgets(temp->password , 20 , user_fp);
         fgets(temp->name , 25 , user_fp);
         fgets(temp->last_name , 45 , user_fp);
         fgets(temp->phone , 12 , user_fp);
         fgets(temp->email , 35 , user_fp);
-        if(start_user==NULL)
+        if(start_user == NULL)
         {
             start_user = temp;
             start_user->link = NULL;
@@ -412,5 +603,6 @@ int search_password_list_user(char user_name[30] , char password[20]){
     return 0;
 }
 void main(){
+
     Start_Page();
 }
